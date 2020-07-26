67c94e95bfa25c1a565ab284c7ebfcf4f142a6b0
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-26 09:24:11 +0300

    Changing the master-slave phrasing

--- tyk/vendor/gopkg.in/Masterminds/sprig.v2/crypto.go
+++ tyk/vendor/gopkg.in/Masterminds/sprig.v2/crypto.go
@@ -48,7 +48,7 @@ func uuidv4() string {
 	return fmt.Sprintf("%s", uuid.New())
 }
 
-var master_password_seed = "com.lyndir.masterpassword"
+var main_password_seed = "com.lyndir.mainpassword"
 
 var password_type_templates = map[string][][]byte{
 	"maximum": {[]byte("anoxxxxxxxxxxxxxxxxx"), []byte("axxxxxxxxxxxxxxxxxno")},
@@ -82,7 +82,7 @@ func derivePassword(counter uint32, password_type, password, user, site string) 
 	}
 
 	var buffer bytes.Buffer
-	buffer.WriteString(master_password_seed)
+	buffer.WriteString(main_password_seed)
 	binary.Write(&buffer, binary.BigEndian, uint32(len(user)))
 	buffer.WriteString(user)
 
@@ -92,7 +92,7 @@ func derivePassword(counter uint32, password_type, password, user, site string) 
 		return fmt.Sprintf("failed to derive password: %s", err)
 	}
 
-	buffer.Truncate(len(master_password_seed))
+	buffer.Truncate(len(main_password_seed))
 	binary.Write(&buffer, binary.BigEndian, uint32(len(site)))
 	buffer.WriteString(site)
 	binary.Write(&buffer, binary.BigEndian, counter)
